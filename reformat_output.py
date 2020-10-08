d1e22ae533a402ceaebce774587b9abf4e2199ef
sedrickkeh <sedrickkeh@gmail.com>
2020-10-08 05:46:05 -0400

    fix reformat_output file

--- reformat_output.py
+++ reformat_output.py
@@ -1,17 +1,36 @@
+import os
+import argparse
 import json
 
-preds = []
-for line in open('output/stackexchange_2_2/pred/stackexchange_2.one2seq.cdkgen_step_10000/stackexchange_2.pred', 'r'):
-    output = json.loads(line)
-    pred_sents = output["pred_sents"]
-    pred = ""
-    for sent in pred_sents:
-        hashtag = ""
-        for word in sent: hashtag = hashtag + word + " "
-        hashtag = hashtag.strip()
-        pred = pred + ";" + hashtag
-    preds.append(pred[1:])
+def reformat(model, steps):
+    preds = []
+    filepath = "output/{}/pred/stackexchange_step_{}/stackexchange_2.pred".format(model, steps)
+    for line in open(filepath, 'r'):
+        output = json.loads(line)
+        pred_sents = output["pred_sents"]
+        pred = ""
+        for sent in pred_sents:
+            hashtag = ""
+            for word in sent: hashtag = hashtag + word + " "
+            hashtag = hashtag.strip()
+            pred = pred + ";" + hashtag
+        preds.append(pred[1:])
 
-with open('output/stackexchange_2_2/pred/stackexchange_2.one2seq.cdkgen_step_10000/stackexchange_2_processed.pred', 'w') as f:
-    for pred in preds:
-        f.write("%s\n" % pred)
+    savepath = "output/{}/pred/stackexchange_step_{}/stackexchange_processed.pred".format(model, steps)
+    with open(savepath, 'w') as f:
+        for pred in preds:
+            f.write("%s\n" % pred)
+
+
+def main(config):
+    model = config.model.lower()
+    steps = config.steps
+    reformat(model, steps)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="stackexchange_0")
+    parser.add_argument('--steps', type=str, default="10000")
+    config = parser.parse_args()
+
+    main(config)
