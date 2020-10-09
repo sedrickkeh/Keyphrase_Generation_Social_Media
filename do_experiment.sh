bd6550e6b15ae1a47c3800ef1603a71dc712b81a
sedrickkeh <sedrickkeh@gmail.com>
2020-10-09 00:19:31 -0400

    make bash file general

--- do_experiment.sh
+++ do_experiment.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 # Set experiment name
-if (( $# < 1 )); then
-    echo "Usage: ./do_experiment.sh [name_of_experiment] [hyperparams], e.g. ./do_experiment.sh stackexchange_0 \"--dropout=0.2\""
+if (( $# < 2 )); then
+    echo "Usage: ./do_experiment.sh [task] [name_of_experiment] [hyperparams], e.g. ./do_experiment.sh stackexchange_2 stackexchange_0 \"--dropout=0.2\""
     exit
 fi
 
 # Set variables
-name=$1
-hparams=$2
-echo "Hyperparameters: $2"
+task=$1
+name=$2
+hparams=$3
+echo "Hyperparameters: $hparams"
 
 # Train
 echo "========= Training ============="
 python train.py \
-    -config config/train/config-transformer-stackexchange.yml \
+    -config config/train/config-transformer-$task.yml \
     -exp ${name} \
-    -save_model models/${name}/stackexchange \
-    -log_file models/${name}/stackexchange.log \
+    -save_model models/${name}/$task \
+    -log_file models/${name}/$task.log \
     -tensorboard_log_dir  runs/${name}/ \
     $hparams
 
@@ -34,7 +35,7 @@ python kp_gen_eval.py \
     -data_dir data/keyphrase/meng17/ \
     -ckpt_dir ./models/${name}/ \
     -output_dir output/${name}/ \
-    -testsets stackexchange_2 \
+    -testsets $task \
     -gpu 0 --verbose \
     --beam_size 10 --batch_size 32 \
     --max_length 40 --onepass \
